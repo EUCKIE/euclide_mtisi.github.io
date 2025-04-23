1ef75dfce4be7f86a4c272b3a455464bb46c723f
EUCKIE <euclidemtisi@gmail.com>
2025-04-24 00:26:09 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navBar = document.querySelector('.nav-bar');
+
+menuIcon.onclick=() => {
+    navBar.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+    
+}
