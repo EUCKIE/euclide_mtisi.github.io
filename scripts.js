30557ecb1513b76d6f2b7570e297e525a0a30b0b
EUCKIE <euclidemtisi@gmail.com>
2025-05-02 18:46:46 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,3 +7,27 @@ menuIcon.onclick=() => {
     
 }
 
+// Initialize EmailJS with your User ID
+emailjs.init('kf9R1lst7Uofy_OUJ');
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Store the form element in a variable
+    const form = e.target;
+    
+    // Send email
+    emailjs.send('service_5pngxel', 'template_hq1pkh6', {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('number').value,
+        message: document.getElementById('message').value
+    })
+    .then(() => {
+        alert('Email sent successfully!');
+        form.reset(); // Clear all input fields after successful submission
+    })
+    .catch(() => {
+        alert('Failed to send email.');
+    });
+});
